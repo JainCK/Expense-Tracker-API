0e259ddbfcdf730456d31fac894e89ad1cbe5de5
Jain Kuriakose <kuriakosejain456@gmail.com>
2024-08-08 22:19:29 +0530

    import updated and dotenv fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './utils/db';
+import authRoutes from './routes/authRoute'
+import expenseRoute from './routes/expenseRoute'
 
-
+dotenv.config();
 
 const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/expenses', expenseRoute);
 
 
 app.listen(3000, function () {
